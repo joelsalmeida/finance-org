5ef65b42054a69752247e6b4859357f859bfe240
Joel Almeida <joel.almeida.sa@gmail.com>
2025-04-28 14:25:19 -0300

    feat: Enhance AppController with authentication and user profile retrieval methods

--- apps/finance-back/src/app/app.controller.ts
+++ apps/finance-back/src/app/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Get,
+  Body,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { AuthService } from './auth/application/services/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { GetUserByEmailInput, GetUserByEmailUseCase } from './user/ports/in';
+import { User } from './user/domain/user.domain';
+import { AuthenticateInput } from './auth/ports/in';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService,
+    private getUserByEmailService: GetUserByEmailUseCase
+  ) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @UseGuards(LocalAuthGuard)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @Post('auth/login')
+  async login(
+    @Body() loginInput: AuthenticateInput
+  ): Promise<{ access_token: string }> {
+    const loginCommand = loginInput.toCommand();
+    return this.authService.generateAccessToken(loginCommand);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @Get('profile')
+  async getProfile(
+    @Body() getProfileInput: GetUserByEmailInput
+  ): Promise<Pick<User, 'id' | 'email'>> {
+    const getUserByEmailCommand = getProfileInput.toCommand();
+
+    const userFound = await this.getUserByEmailService.getUserByEmail(
+      getUserByEmailCommand
+    );
+
+    return { id: userFound.id, email: userFound.email };
   }
 }
--- apps/finance-back/src/app/app.module.ts
+++ apps/finance-back/src/app/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SaveUserService } from './user/application/services/seve-user.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserEntity } from './user/adapter/out';
+import { GetUserByEmailService } from './user/application/services/get-user-by-email.service';
+import { GetUserByEmailUseCase } from './user/ports/in';
+import { env } from 'process';
+import { ConfigModule } from '@nestjs/config';
+import { Dialect } from 'sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    SequelizeModule.forRoot({
+      dialect: env.DATABASE_DIALECT as Dialect,
+      host: env.DATABASE_HOST,
+      port: Number(env.DATABASE_PORT),
+      username: env.DATABASE_USERNAME,
+      password: env.DATABASE_PASSWORD,
+      database: env.DATABASE,
+      models: [UserEntity],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    SaveUserService,
+    { provide: GetUserByEmailUseCase, useClass: GetUserByEmailService },
+  ],
 })
 export class AppModule {}
