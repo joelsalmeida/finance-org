3b031b13463e904fa7fa6eba56cf8096b13e9b9b
Joel Almeida <joel.almeida.sa@gmail.com>
2025-04-30 20:29:13 -0300

    refactor: Update exception codes and modify constructor visibility in DomainBaseException

--- apps/finance-back/src/exceptions/domain-base.exception.ts
+++ apps/finance-back/src/exceptions/domain-base.exception.ts
@@ -1,7 +1,7 @@
 export abstract class DomainBaseException extends Error {
   abstract readonly code: string;
 
-  constructor(message: string) {
+  protected constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
   }
