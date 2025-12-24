df55db622027456a3d11b2bf2760f128f286164d
Joel Almeida <joel.almeida.sa@gmail.com>
2025-12-24 10:43:23 -0300

    fix: Adds missing attributes to budget

--- apps/finance-back/src/modules/budget/domain/budget.domain.ts
+++ apps/finance-back/src/modules/budget/domain/budget.domain.ts
@@ -1,20 +1,33 @@
-import { Category, Money } from '../../../value-objects';
+import {
+  AccountNumber,
+  BudgetId,
+  Category,
+  Money,
+} from '../../../value-objects';
 
 export type BudgetAttributes = {
+  id: BudgetId;
+  accountNumber: AccountNumber;
   category: Category;
   limit: Money;
+  spent: Money;
 };
 
 export class Budget {
+  private readonly _id: BudgetId;
+  private readonly _accountNumber: AccountNumber;
   private readonly _category: Category;
   private readonly _limit: Money;
   private _spent: Money;
 
   private constructor(budgetAttributes: BudgetAttributes) {
-    const { category, limit } = budgetAttributes;
+    const { id, accountNumber, category, limit, spent } = budgetAttributes;
 
+    this._id = id;
+    this._accountNumber = accountNumber;
     this._category = category;
     this._limit = limit;
+    this._spent = spent;
   }
 
   static create(budgetAttributes: BudgetAttributes) {
@@ -24,6 +37,14 @@ export class Budget {
   addSpend(amount: Money): Money {
     this._spent = this._spent.add(amount);
     return this._spent;
+  }
+
+  get id(): BudgetId {
+    return this._id;
+  }
+
+  get accountNumber(): AccountNumber {
+    return this._accountNumber;
   }
 
   get category(): Category {
