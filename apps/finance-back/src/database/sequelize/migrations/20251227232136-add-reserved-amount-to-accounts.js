a93423f1c7b1b46b46eeafff87820cf591f62364
Joel Almeida <joel.almeida.sa@gmail.com>
2025-12-29 11:11:54 -0300

    feat(db): add reservedAmount column to accounts table

---
+++ apps/finance-back/src/database/sequelize/migrations/20251227232136-add-reserved-amount-to-accounts.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('accounts', 'reservedAmount', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('accounts', 'reservedAmount');
+  },
+};
