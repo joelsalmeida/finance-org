5c77f4b07ad4108c22fc8cf81bca0359d57b7c4d
Joel Almeida <joel.almeida.sa@gmail.com>
2026-01-01 10:10:45 -0300

    feat(transfer): Implements error cases for accounts not found and adds a created transaction event

--- apps/finance-back/src/modules/transfer/application/services/transfer.service.ts
+++ apps/finance-back/src/modules/transfer/application/services/transfer.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { AccountNumber, Category, Money } from '../../../../value-objects';
 import { CategoryNameType } from '../../../../value-objects/category/index.types';
 import { Account } from '../../../account/domain/account.domain';
@@ -10,8 +11,13 @@ import {
 } from '../../../shared/application/ports/out';
 import { TransactionFactoryInterface } from '../../../transaction/application/factories/transaction.factory.types';
 import { Transaction } from '../../../transaction/domain';
+import { TransactionCreatedEvent } from '../../../transaction/domain/events';
 import { TransactionPersistencePort } from '../../../transaction/ports/out';
 import { TransferCommand } from '../commands';
+import {
+  DestinationAccountNotFoundException,
+  SourceAccountNotFoundException,
+} from '../exceptions';
 import { TransferUseCase } from '../use-cases';
 
 type AccountNumbers = { source: string; destination: string };
@@ -21,7 +27,7 @@ type LoadAccountsParam = {
   options: UowOptions;
 };
 
-type PersistenceData = {
+type TransferPersistenceContext = {
   sourceAccount: Account;
   destinationAccount: Account;
   transaction: Transaction;
@@ -34,81 +40,78 @@ export class TransferService implements TransferUseCase {
     private accountPersistencePort: AccountPersistencePort,
     private transactionPersistencePort: TransactionPersistencePort,
     @Inject('transactionFactory')
-    private transactionFactory: TransactionFactoryInterface
+    private transactionFactory: TransactionFactoryInterface,
+    private eventEmitter: EventEmitter2
   ) {}
 
   async execute(command: TransferCommand): Promise<Transaction> {
-    const { sourceAccountNumber, destinationAccountNumber, amount, category } =
-      command;
-
     return this.uow.execute(async (options) => {
       const WRITE_OPTIONS: UowOptions = this.withWriteOptions(options);
-
-      const accountNumbers = {
-        source: sourceAccountNumber,
-        destination: destinationAccountNumber,
-      };
-
-      const { sourceAccount, destinationAccount } = await this.loadAccounts({
-        accountNumbers,
-        options: WRITE_OPTIONS,
-      });
-
-      const transactionValue = Money.fromCents(amount);
-
-      sourceAccount.withdraw(transactionValue);
-      destinationAccount.deposit(transactionValue);
-
-      const transaction = this.createTransactionOrThrow(
-        accountNumbers,
-        category,
-        transactionValue
-      );
-
-      const persistenceData = {
-        sourceAccount,
-        destinationAccount,
-        transaction: transaction.data,
-      };
-
-      await this.persist(persistenceData, WRITE_OPTIONS);
-      return transaction.data;
+      return this.performTransfer(command, WRITE_OPTIONS);
     });
   }
 
-  private async persist(persistenceData: PersistenceData, options: UowOptions) {
-    await this.accountPersistencePort.save(
-      persistenceData.sourceAccount,
-      options
-    );
-    await this.accountPersistencePort.save(
-      persistenceData.destinationAccount,
-      options
-    );
-    await this.transactionPersistencePort.save(
-      persistenceData.transaction,
-      options
-    );
-  }
+  private async performTransfer(command: TransferCommand, options: UowOptions) {
+    const { sourceAccountNumber, destinationAccountNumber, amount, category } =
+      command;
 
-  private withWriteOptions(options: UowOptions): UowOptions {
-    return {
-      transactionContext: options.transactionContext,
-      lockMode: LOCK_MODE.WRITE,
+    const accountNumbers = {
+      source: sourceAccountNumber,
+      destination: destinationAccountNumber,
     };
+
+    const { sourceAccount, destinationAccount } =
+      await this.loadTransferAccounts({
+        accountNumbers,
+        options,
+      });
+
+    const transactionValue = Money.fromCents(amount);
+
+    sourceAccount.withdraw(transactionValue);
+    destinationAccount.deposit(transactionValue);
+
+    const transaction = this.createTransactionOrThrow(
+      accountNumbers,
+      category,
+      transactionValue
+    );
+
+    const context: TransferPersistenceContext = {
+      sourceAccount,
+      destinationAccount,
+      transaction,
+    };
+
+    await this.persistTransfer(context, options);
+
+    this.publishTransactionCreated(transaction);
+
+    return transaction;
   }
 
-  private async loadAccounts({ accountNumbers, options }: LoadAccountsParam) {
+  private async loadTransferAccounts({
+    accountNumbers,
+    options,
+  }: LoadAccountsParam) {
     const sourceAccount = await this.accountPersistencePort.findByAccountNumber(
       accountNumbers.source,
       options
     );
+
+    if (!sourceAccount) {
+      throw new SourceAccountNotFoundException(accountNumbers.source);
+    }
 
     const destinationAccount =
       await this.accountPersistencePort.findByAccountNumber(
         accountNumbers.destination,
         options
       );
+
+    if (!destinationAccount) {
+      throw new DestinationAccountNotFoundException(accountNumbers.destination);
+    }
 
     return { sourceAccount, destinationAccount };
   }
@@ -137,6 +140,37 @@ export class TransferService implements TransferUseCase {
       throw transaction.error;
     }
 
-    return transaction;
+    return transaction.data;
+  }
+
+  private async persistTransfer(
+    context: TransferPersistenceContext,
+    options: UowOptions
+  ) {
+    // TODO: Refactor this implementation and tests to use bulk save
+    await this.accountPersistencePort.save(context.sourceAccount, options);
+    await this.accountPersistencePort.save(context.destinationAccount, options);
+    await this.transactionPersistencePort.save(context.transaction, options);
+  }
+
+  private publishTransactionCreated(eventPayload: Transaction) {
+    this.eventEmitter.emit(
+      TransactionCreatedEvent.name,
+      new TransactionCreatedEvent(
+        eventPayload.id,
+        eventPayload.sourceAccountNumber,
+        eventPayload.destinationAccountNumber,
+        eventPayload.category,
+        eventPayload.amount,
+        new Date()
+      )
+    );
+  }
+
+  private withWriteOptions(options: UowOptions): UowOptions {
+    return {
+      transactionContext: options.transactionContext,
+      lockMode: LOCK_MODE.WRITE,
+    };
   }
 }
